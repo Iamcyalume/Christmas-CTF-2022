e0fe2d7bb2d963be9a668acbe332e7a0d248fe0e
cyalume <cyalumeJ@gmail.com>
2022-12-28 12:25:41 +0900

    Add files via upload

---
+++ Light Up My Christmas!/code/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace _ChristmasCTF__Light_Up_My_Christmas_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(773, 396);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(412, 28);
+            this.textBox.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1110, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.BackgroundImage = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.tree;
+            this.pictureBox7.Image = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.snwman_off;
+            this.pictureBox7.Location = new System.Drawing.Point(502, 250);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(193, 164);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.BackgroundImage = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.tree;
+            this.pictureBox6.Image = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.ru_off;
+            this.pictureBox6.Location = new System.Drawing.Point(325, 352);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(205, 185);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.BackgroundImage = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.tree;
+            this.pictureBox5.Image = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.sock_off;
+            this.pictureBox5.Location = new System.Drawing.Point(502, 420);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(256, 227);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.tree;
+            this.pictureBox4.Image = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.cane_off;
+            this.pictureBox4.Location = new System.Drawing.Point(459, 598);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(154, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.tree;
+            this.pictureBox3.Image = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.bulb_off;
+            this.pictureBox3.Location = new System.Drawing.Point(269, 506);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(200, 188);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.tree;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox2.Image = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.star_off;
+            this.pictureBox2.Location = new System.Drawing.Point(391, -23);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(292, 294);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.tree1;
+            this.pictureBox1.Location = new System.Drawing.Point(-54, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1136, 1113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::_ChristmasCTF__Light_Up_My_Christmas_.Properties.Resources.landscape;
+            this.ClientSize = new System.Drawing.Size(1362, 830);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Light Up My Christmas!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Light Up My Christmas!/code/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+
+//christmasCTF{cEr7iF13d_ChristmaS_F1Ag}
+
+namespace _ChristmasCTF__Light_Up_My_Christmas_
+{
+    public partial class Form1 : Form
+    {
+        string input;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.BackColor = Color.Transparent;
+            // pictureBox1.Parent = 
+
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Parent = pictureBox1;
+
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.Parent = pictureBox1;
+
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.Parent = pictureBox1;
+
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox5.Parent = pictureBox1;
+
+            pictureBox6.BackColor = Color.Transparent;
+            pictureBox6.Parent = pictureBox1;
+
+            pictureBox7.BackColor = Color.Transparent;
+            pictureBox7.Parent = pictureBox1;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            input = null;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) //tree
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e) //star
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e) //bulb
+        {
+
+        }
+        private void pictureBox4_Click(object sender, EventArgs e) //cane
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e) //sock
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e) //ru
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e) //snwman
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e) //snwman
+        {
+            input = null;
+            input = textBox.Text;
+            if (!Bulb(input))
+            {
+                return;
+            }
+
+            Validation(input);
+
+        }
+
+        private bool Validation(string input)
+        {
+            if(Star(Cane(input) && Sock(input) && Ru(input) && Snwman(input)))
+            {
+                MessageBox.Show("Merry Christmas!");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool Bulb(string input) // length
+        {
+            if (input.Length != 0x26)
+                return false;
+            else pictureBox3.Image = Properties.Resources.bulb_on;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            return true;
+        }
+
+        private bool Cane(string input) // flag format
+        {
+            if (input.Contains("christmasCTF{") && input.Contains("}")
+                && (input[input.Length - 1] == '}'))
+            {
+                pictureBox4.Image = Properties.Resources.cane_on;
+                return true;
+            }
+            return false;
+
+        }
+
+        private bool Sock(string input) // equation 1 ~ (christmas) base64 구현, christmaS_
+        {
+            byte[] data = new byte[input.Length];
+            for(int i = 23; i<33; i++)
+            {
+                data[i-23] = (byte)input[i];
+            }
+
+            string str = null;
+
+            for(int i = 0; i<((input.Length * 30 - input.Length ^ 1233) / 87 + input.Length * 2 - 27); i++)
+            {
+                str = Convert.ToBase64String(data);
+            }
+            
+            if(str == "Q2hyaXN0bWFTXwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
+            {
+                pictureBox5.Image = Properties.Resources.sock_on;
+                return true;
+            }
+            return false;
+        }
+
+
+        private bool Ru(string input) // 
+        {
+            char[] str = new char[4];
+            int[] arr = new int[4] {2, 2, 4, 53};
+            char[] deer = new char[] { 'D', '3', 'E', 'R' };
+
+            for(int i = 33; i<37; i++)
+            {
+                str[i-33] = input[i];
+            }
+
+            for(int i = 0; i<4; i++)
+            {
+                if((str[i]^deer[i]) != arr[i])
+                {
+                    return false;
+                }
+            }
+
+            pictureBox6.Image = Properties.Resources.ru_on;
+            return true;
+        }
+
+        private bool Snwman(string input)
+        {
+            int i;
+            char[] str = new char[38];
+            int[] arr = new int[] { 99, 68, 112, 52, 101, 65, 43, 44, 92, 86 };
+
+            for (i = 0; i < input.Length; i++)
+            {
+                str[i] = input[i];
+            }
+
+            for (i = 13; i < 23; i++)
+            {
+                if (
+                    ((str[i] - (i - 13) + ((((str[i] >> i)) | ((str[i] << 8 - i) & 255)) ^
+                   (((str[i] << i) & 255) | str[i] >> 8 - i))) != arr[i-13])
+                    )
+                {
+                    return false;
+
+                }else{
+                    continue;
+                }
+            }
+
+            pictureBox7.Image = Properties.Resources.snwman_on;
+            return true;
+
+        }
+
+
+        private bool Star(bool flags) // if all ornaments has true value - &&
+        {
+            if(flags == true)
+            {
+                pictureBox2.Image = Properties.Resources.star_on;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
